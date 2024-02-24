3ff7ab87bd15348389675b7c9028082f8c04bf27
phunn <phuteobs0166@gmail.com>
2024-02-24 21:44:21 +0700

    wip

---
+++ src/DistributedSystem.Persistence/Migrations/20240221140651_Add_Process_on_utc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DistributedSystem.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Process_on_utc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ProcessOnUtc",
+                table: "OutboxMessages",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProcessOnUtc",
+                table: "OutboxMessages");
+        }
+    }
+}
